4d3dcf1f5d00f6df7aa5e852a21b2d7b33ded822
Jason McAlpin <jason@chaiboy.com>
2012-10-22 15:30:07 -0300

    optimize branding
    Optimize code to just show a branded frame for external URL

--- framebranding.js
+++ framebranding.js
@@ -1,7 +1,7 @@
 // JavaScript Document
 function pagePreview(actualurl) {
-//brandingframe.close();
 var framewin = window.open("","brandingframe");
+
 framewin.document.writeln('<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">'
  +'<html xmlns="http://www.w3.org/1999/xhtml">'
  +'<head><meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />'
@@ -22,28 +22,7 @@ void(0);
 
 $(document).ready(function() {
 						   
-						   
-						   $("#wrapper").animate({
-							  opacity: 0
-							  },0).animate({
-								opacity: 1
-							  },1000);
-	
-	
-	
-	$("#wrapper").removeClass("hide").addClass("show");
-	
-	$("#nav a,#sidebar a").click(function(event){
-		event.preventDefault();
-		linkLocation = this.href;
-		
-		$("#wrapper").animate({
-								opacity: 0
-							  },500, redirectPage);		
-		
-		
-		
-	});
+
 
 	
 	$(".post a").click(function(event){
@@ -63,37 +42,6 @@ window.location = linkLocation;
 	} //functon end
 ); //ready end
 
-
-
-
-function highlight(targetEl){
-	$(targetEl).animate({backgroundColor: 'red'},500);
-	$(targetEl).addClass("live");
-}
-
-function kickit(){
-	  $("#wrapper").animate({
-							  opacity: 0,
-							  left:"1000px"
-							  },0).animate({
-								opacity: 1,
-							  	left:"0px"
-							  },1000);
-	$("#wrapper").removeClass("hide").addClass("show");
-	
-	$("a").click(function(event){
-		event.preventDefault();
-		linkLocation = this.href;
-		$("#wrapper").animate({
-								opacity: 0,
-							  	left:"-1000px"
-							  },1000, redirectPage);		
-	});
-		
-	function redirectPage() {
-		window.location = linkLocation;
-	}
-	}
 	
 	
 	
